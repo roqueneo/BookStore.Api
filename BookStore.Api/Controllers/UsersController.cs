c7e03b758c5643898ab09bcfae8ade3de856c783
Alonso Roque A <alonso.roque@apsys.com.mx>
2020-12-01 00:53:50 -0600

    Implement Login endpoint

---
+++ BookStore.Api/Controllers/UsersController.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using BookStore.Api.Contracts;
+using BookStore.Api.DTOs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace BookStore.Api.Controllers
+{
+    /// <summary>
+    /// Controller used to interact with the Users in the book store's database.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class UsersController : BaseController
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UsersController(ILoggerService logger, IMapper mapper, SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager) 
+            : base(logger, mapper)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Login([FromBody] UserDto userDto)
+        {
+            try
+            {
+                var username = userDto.Username;
+                var password = userDto.Password;
+                var result = await _signInManager.PasswordSignInAsync(username, password, false, false);
+                if (result.Succeeded)
+                {
+                    var user = await _userManager.FindByNameAsync(username);
+                    return Ok(user);
+                }
+
+                return Unauthorized(userDto);
+            }
+            catch (Exception ex)
+            {
+                return LogErrorAndBuildInternalError(ex, $"Somethig went wrong login with user [{userDto.Username}]. Please contact the Administrator");
+            }
+        }
+    }
+}
---
+++ BookStore.Api/DTOs/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace BookStore.Api.DTOs
+{
+    public class UserDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
